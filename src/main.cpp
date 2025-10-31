99d445771880de716d343db35f01f7b97de6738c
Christiana Eberle <christiana.eberle@janitza.de>
2025-10-31 11:37:41 +0100

    LED leuchtet

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <Arduino.h>
 
+// Wir nehmen an, die LED ist an GPIO25 angeschlossen
+int ledPin = 25;  // Pin-Nummer für die LED
+
 void setup() {
+  // Pin als Ausgang einstellen
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+  // LED einschalten
+  digitalWrite(ledPin, HIGH);
 }
