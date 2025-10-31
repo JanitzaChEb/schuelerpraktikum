265fe6f295c4fbd2aeca104e91bc1176d8329c53
Christiana Eberle <christiana.eberle@janitza.de>
2025-10-31 11:46:37 +0100

    LED blinkt

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <Arduino.h>
 
-// Wir nehmen an, die LED ist an GPIO25 angeschlossen
-int ledPin = 25;  // Pin-Nummer für die LED
+// LED ist an GPIO25 angeschlossen
+int ledPin = 25;
 
 void setup() {
-  // Pin als Ausgang einstellen
+  // Pin als Ausgang festlegen
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // LED einschalten
   digitalWrite(ledPin, HIGH);
+  delay(500);  // warte 500 Millisekunden (0,5 Sekunden)
+
+  // LED ausschalten
+  digitalWrite(ledPin, LOW);
+  delay(500);  // wieder 0,5 Sekunden warten
 }
